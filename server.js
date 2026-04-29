b2431239bf8de9a02181c3a81c45ff93e6e04301
Lalit Patil <patillalit125@gmail.com>
2026-04-29 17:39:38 +0530

    fix versal backend

--- server.js
+++ server.js
@@ -2,74 +2,52 @@ const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
+
 app.use(cors());
 app.use(bodyParser.json());
+app.use(express.static(__dirname));
 
 // MySQL Connection
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'plumbing_db'
+    host: 'YOUR_DB_HOST',
+    user: 'YOUR_DB_USER',
+    password: 'YOUR_DB_PASSWORD',
+    database: 'YOUR_DB_NAME'
 });
 
 db.connect(err => {
     if (err) {
-        console.error('Database Error: ' + err.message);
+        console.error(err);
         return;
     }
-    console.log('Connected to MySQL Database!');
+    console.log('DB Connected');
 });
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 app.get('/api/plumbers', (req, res) => {
-    db.query("SELECT * FROM plumbers", (err, result) => {
+    db.query('SELECT * FROM plumbers', (err, result) => {
         if (err) return res.status(500).send(err);
         res.send(result);
     });
 });
-
 
 app.post('/api/book', (req, res) => {
     const { custName, custMobile, custAddress, plumberName } = req.body;
-    const sql = "INSERT INTO bookings (custName, custMobile, custAddress, plumberName) VALUES (?, ?, ?, ?)";
-    
+
+    const sql = `INSERT INTO bookings 
+    (custName, custMobile, custAddress, plumberName) 
+    VALUES (?, ?, ?, ?)`;
+
     db.query(sql, [custName, custMobile, custAddress, plumberName], (err, result) => {
         if (err) return res.status(500).send(err);
-        
-        db.query("UPDATE plumbers SET status = 'Booked' WHERE name = ?", [plumberName]);
-        res.send({ message: "Success", id: result.insertId });
+        res.send({ message: 'Success' });
     });
 });
 
-
-app.get('/api/admin/bookings', (req, res) => {
-    db.query("SELECT * FROM bookings ORDER BY id DESC", (err, result) => {
-        if (err) return res.status(500).send(err);
-        res.send(result);
-    });
-});
-
-
-app.post('/api/admin/update-status', (req, res) => {
-    const { name, status } = req.body;
-    db.query("UPDATE plumbers SET status = ? WHERE name = ?", [status, name], (err, result) => {
-        if (err) return res.status(500).send(err);
-        res.send({ message: "Updated" });
-    });
-});
-
-app.listen(3000, () => console.log(`Server: http://localhost:3000`));
-
-const express = require('express');
-const path = require('path');
-const app = express();
-
-app.use(express.static(__dirname));
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
-
+module.exports = app;
